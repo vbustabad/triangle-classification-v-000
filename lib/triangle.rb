67f62e6d2e5eef656add072c96197899b4d6e3f7
Veronica Bustabad <vbustabad@gmail.com>
2018-04-14 02:13:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  attr_reader :length_one, :length_two, :length_three
+
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end
+
+  def kind
+    if length_one == length_two && length_two == length_three && length_one == length_three
+      return :equilateral
+    elsif length_one != length_two && length_two != length_three && length_one != length_three
+      return :scalene
+    else
+      return :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end

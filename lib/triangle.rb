90df60e2bff3b02dbc608edc3a8fe7e37a17f3c9
Veronica Bustabad <vbustabad@gmail.com>
2018-04-14 02:44:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,25 +1,26 @@
 class Triangle
-  attr_reader :length_one, :length_two, :length_three
+  attr_reader :a, :b, :c
 
-  def initialize(length_one, length_two, length_three)
-    @length_one = length_one
-    @length_two = length_two
-    @length_three = length_three
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    if length_one == length_two && length_two == length_three
-      return :equilateral
-    elsif length_one == length_two || length_two == length_three || length_one == length_three
-      return :isosceles
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
     else
-      return :scalene
+      :scalene
     end
   end
 
   def validate_triangle
-    real_triangle = [(length_one + length_two > length_three), (length_one + length_three > length_two), (length_two + length_three > length_one)]
-    [length_one, length_two, length_three].each { |side| real_triangle << false if side <= 0 }
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
     raise TriangleError if real_triangle.include?(false)
   end
 
